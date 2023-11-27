492de6e74cfa2d60283115670ddc8105026747c4
Elizaveta Psar <105010678+Psar-Elizaveta@users.noreply.github.com>
2023-11-27 12:38:18 +0200

    add

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class PhoneError(Exception):
     pass
 
 class Field:
-    def __init__(self, value) -> None:
+    def __init__(self, value):
         self.value = value
 
     def __str__(self):
@@ -29,13 +29,17 @@ class Name(Field):
             raise NameTooShortError('Name is too short, need more than 3 symbols. Try again.')
         if len(name) > 10:
             raise NameTooLongError('Name is too long, need only 10 symbols. Try again.')
+        return name
 
 class Phone(Field):
-    def validation(self, value):
+    def __init__(self, value):
         super().__init__(value)
-        # self.value = value
-        if len(value) != 10 and not value.isdigit():
-            raise PhoneError('Not a number!')
+        # self.validation(value)
+    # def validation(self, value):
+    #     super().__init__(value)
+    #     # self.value = value
+        if len(value) != 10 or not value.isdigit():
+            raise ValueError
         
         # elif len(value) == 10 and value.isdigit():
         #     return value
@@ -44,7 +48,7 @@ class Phone(Field):
                     
 class Record:
     def __init__(self, name):
-        self.name = Name(name)
+        self.name = Field(name)
         self.phones = []
         
     def __str__(self) -> str:
@@ -52,34 +56,36 @@ class Record:
 
     def add_phone(self, phone_number):
         phone = Phone(phone_number)
-        phone.validation(phone_number)
+        # phone.validation(phone_number)
         if phone not in self.phones:
-            self.phones.append(phone_number)
+            self.phones.append(phone)
+    
     
     def remove_phone(self, phone):
         for ph in self.phones:
-            if str(ph) == phone:
-                self.phones.remove(phone)
-                break
+            if isinstance(ph, Field) and ph.value == phone:
+                self.phones.remove(ph)
             
-    def edit_phone(self, old_phone, new_phone):
-        for phone in self.phones:
-            if old_phone == phone:
-                index_phone = self.phones.index(old_phone)
-                self.phones[index_phone] = new_phone
-                break 
+
 
     
     def find_phone(self, search_phone):
         for phone in self.phones:
-            if str(phone) == search_phone:
-                return search_phone
-        return 'Not such phone'
-
+            if isinstance(phone, Field) and phone.value == search_phone:
+                return phone.value
+        return None
+    
     def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+        return f"Contact name: {self.name}, phones: {'; '.join(p for p in self.phones)}"
 
-
+        
+    def edit_phone(self, old_phone, new_phone):
+        for phone in self.phones:
+            if old_phone == phone.value:
+                self.phones.remove(phone)
+                self.phones.append(Phone(new_phone))
+                return
+        raise ValueError
 class AddressBook(UserDict):
     # def __init__(self):
     #     se
@@ -91,21 +97,23 @@ class AddressBook(UserDict):
     def find(self, search_name):
         for name, phone  in self.data.items():
             if name == search_name:
-                return record
-        return 'This name isn\'n here'
+                return phone
+        # return self.data.get(name)
+        return None
             
     
     def delete(self, name):
         if name in self.data:
             del self.data[name]
-        else:
-            print('This name isn\'t here')
+            
+    # def delete(self, name):
+    #     return self.data.pop(name)
+        
 
 if __name__ == "__main__":
-# Створення нової адресної книги
+
     book = AddressBook()
 
-        # Створення запису для John
     john_record = Record("John")
     print(john_record)
     john_record.add_phone("1234567890")
@@ -113,29 +121,23 @@ if __name__ == "__main__":
     john_record.add_phone("5555555555")
     print(john_record)
 
-        # Додавання запису John до адресної книги
     book.add_record(john_record)
 
-        # Створення та додавання нового запису для Jane
     jane_record = Record("Jane")
     jane_record.add_phone("9876543210")
     book.add_record(jane_record)
 
-            # Виведення всіх записів у книзі
     for name, record in book.data.items():
         print(record)
 
-            # Знаходження та редагування телефону для John
     john = book.find("John")
     john.edit_phone("1234567890", "1112223333")  #'tuple' object has no attribute 'edit_phone'
 
-    print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+    print(john)  
 
-            # Пошук конкретного телефону у записі John
     found_phone = john.find_phone("5555555555")
     print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
 
-            # Видалення запису Jane
     book.delete("Jane")
 
 
